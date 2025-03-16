ecace83976eb2dadf714ede5a0b98b6706d385af
wbbg <h-schultz@gmx.de>
2025-03-17 00:06:18 +0100

    generate report from JSON files

---
+++ generateReport.py
@@ -0,0 +1,77 @@
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import cm
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus.flowables import BalancedColumns
+
+import json
+import base64
+import argparse
+from io import BytesIO
+# from pprint import pp
+
+
+def getArgs():
+    global args
+    parser = argparse.ArgumentParser(prog="generateReport.py",
+                                     description="generate a PDF from *.ai.json files")
+    parser.add_argument('filename', nargs='+')
+    parser.add_argument('-o', '--outfile', default='aikw.pdf',
+                        help='output file name [%(default)s]')
+    args = parser.parse_args()
+
+
+def getData(fName: str) -> {}:
+    with open(fName, "r") as f:
+        jData = json.load(f)
+    return jData
+
+
+def getImage(data: {}) -> Image:
+    img_bytes = BytesIO(base64.b64decode(data['image_b64']))
+    return Image(img_bytes,
+                 width=7 * cm,
+                 height=7 * cm,
+                 kind='bound'
+                 )
+
+
+def formatResult(data: {}) -> str:
+    return '{} [{:.1f}sec]'.format(data['result'], data['time'])
+
+
+def create_document():
+    global args
+    doc = SimpleDocTemplate(
+        args.outfile,
+        pagesize=A4
+    )
+    styles = getSampleStyleSheet()
+    flowables = []
+    for f in args.filename:
+        data = getData(f)
+        img = getImage(data)
+        flowables.extend([
+            Paragraph(data['filename'],
+                      styles['Code']),
+            Paragraph(formatResult(data['headline']), styles['Heading3']),
+            BalancedColumns([img, Paragraph(formatResult(data['abstract']),
+                                            styles['Normal'])
+                             ]),
+            Spacer(0.5 * cm, 0.5 * cm),
+            Paragraph(formatResult(data['keywords'])),
+            Paragraph("Model: {}".format(data['model']), styles['Italic']),
+            Paragraph("Prompts: ", styles['Heading5']),
+        ])
+        for k in ['headline', 'abstract', 'keywords']:
+            flowables.extend([
+                Paragraph(k, styles['Heading6']),
+                Paragraph(data[k]['prompt'], styles['Definition'])
+            ])
+        flowables.append(PageBreak())
+    doc.build(flowables)
+
+
+if __name__ == '__main__':
+    getArgs()
+    create_document()
