c3809ed6ee070842a473e331ad1768414403b1b9
wbbg <h-schultz@gmx.de>
2025-03-23 12:47:47 +0100

    clean docker-compose.yaml, fix pull-model

--- docker/pull-model.sh
+++ docker/pull-model.sh
@@ -1,6 +1,12 @@
-#! /bin/env zsh
+#! env zsh
 
-MODEL=llava:v1.6
+DEFAULT_MODEL=llava:v1.6
 CONTAINER=ollama
+
+if [[ "$1" == "" ]]; then
+	MODEL=$DEFAULT_MODEL
+else
+	MODEL=$1
+fi
 
 docker exec -it $CONTAINER ollama pull $MODEL
